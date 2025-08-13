271ffdea9de13a4f9cde738862d57aafadfbc5b3
learnuidev <learnuidev@gmail.com>
2025-08-13 12:01:25 -0400

    wip

--- constants/table-names.js
+++ constants/table-names.js
@@ -1,33 +1,19 @@
-const { isLocalEnv } = require("../utils/is-local-env");
+require("dotenv").config();
 
 /* eslint-disable no-undef */
 
-const tableNamesLocal = {
-  mediaTable: "listen-api-dev-MediaTable-1TTNRJRBU6AT2",
-  mediaFilesTable: "listen-api-dev-MediaFilesTable-394TH4YHSPVV",
-  mediaFilesTableV2: "listen-api-dev-MediaFilesTable-394TH4YHSPVV",
-  userAssetsTable: "nomadmethod-api-dev-UserAssetsTable-10ZRO2B9VGOR7",
-
-  booksTable: "listen-api-dev-BooksTable-NPGUL667QXPY",
-  chapterSectionsTable: "listen-api-dev-ChapterSectionsTable-1K5V04WQLPZN2",
-  textToSpeechTable: "listen-api-dev-TextToSpeechTable-RKHUASCCQE4Y",
-  contentAnalyticsTable: "listen-api-dev-ContentAnalyticsTable-CVUKEXHF5VFT",
-  contentsTable: "nomadmethod-api-dev-ContentsTable-NQTY72LN0YJQ",
+const tableNames = {
+  mediaTable: process.env.MEDIA_TABLE,
+  mediaFilesTable: process.env.MEDIA_FILES_TABLE,
+  mediaFilesTableV2: process.env.MEDIA_FILES_TABLE_V2,
+  userAssetsTable: process.env.USER_ASSETS_TABLE,
+  booksTable: process.env.BOOKS_TABLE,
+  chapterSectionsTable: process.env.CHAPTER_SECTIONS_TABLE,
+  textToSpeechTable: process.env.TEXT_TO_SPEECH_TABLE,
+  contentAnalyticsTable: process.env.CONTENT_ANALYTICS_TABLE,
+  contentsTable: process.env.CONTENTS_TABLE,
+  // contentsV2Table: process.env.CONTENTS_TABLE_V2,
 };
-
-const tableNames = isLocalEnv()
-  ? tableNamesLocal
-  : {
-      mediaTable: process.env.MEDIA_TABLE,
-      mediaFilesTable: process.env.MEDIA_FILES_TABLE,
-      mediaFilesTableV2: process.env.MEDIA_FILES_TABLE_V2,
-      userAssetsTable: process.env.USER_ASSETS_TABLE,
-      booksTable: process.env.BOOKS_TABLE,
-      chapterSectionsTable: process.env.CHAPTER_SECTIONS_TABLE,
-      textToSpeechTable: process.env.TEXT_TO_SPEECH_TABLE,
-      contentAnalyticsTable: process.env.CONTENT_ANALYTICS_TABLE,
-      contentsTable: process.env.CONTENTS_TABLE,
-    };
 
 module.exports = {
   tableNames,
